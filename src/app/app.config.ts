01548be7c50e7bb03b0fdb5a39cb7eebcb2a7ef8
Tony H. <tony.hirschligau@googlemail.com>
2025-07-22 10:26:38 +0200

    included new databse for my own

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,5 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), provideFirebaseApp(() => initializeApp({"projectId":"join-7e54b","appId":"1:424943017502:web:0a1d69d4e4a85c3569580f","storageBucket":"join-7e54b.firebasestorage.app","apiKey":"AIzaSyCnn-zzaGhj-Hh20QYqwumfSFlpFGNIyxs","authDomain":"join-7e54b.firebaseapp.com","messagingSenderId":"424943017502"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
+  providers: [provideRouter(routes, withHashLocation()), provideFirebaseApp(() => initializeApp({"projectId": "joinown-7ffa3","appId":"1:188500596893:web:68821393cb9373b4aa9c74","storageBucket":"joinown-7ffa3.firebasestorage.app","apiKey":"AIzaSyAeRhSKm4aCoZ2qlT4uYMl6tTffA1lFgjE","authDomain":"joinown-7ffa3.firebaseapp.com","messagingSenderId":"188500596893"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
 };
